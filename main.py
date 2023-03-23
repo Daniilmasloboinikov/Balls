501ae7d3edfc7e74f06c4364d830fabc10d28249
Daniilmasloboinikov <Daniilmasloboinikov@mail.ru>
2023-03-24 00:29:22 +0300

    Исправлены баги с отталкиванием шаров

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from random import randint
-from math import fabs
 
 #Размер окна
 sqreen_X = 500
@@ -9,10 +8,10 @@ sqreen_Y = 500
 class Ball():
 
     def __init__(self):
-        self.R = randint(20,30)
+        self.R = randint(30,50)
         self.x = randint(10, sqreen_X-self.R)
         self.y = self.x
-        self.dx,self.dy = (randint(-10,10),randint(-10,10))
+        self.dx,self.dy = (randint(-1,1),1)
         self.ball_id = canvas.create_oval(self.x - self.R,
                                           self.y - self.R,
                                           self.x + self.R,
@@ -22,18 +21,19 @@ class Ball():
     def move(self):
         for ball in balls:
             if self != ball:
-                distance = fabs((pow(self.x,2)+pow(self.y,2))**(0.5)-(pow(ball.x,2)+pow(ball.y,2))**(0.5))
+                distance = (pow(self.x-ball.x,2)+pow(self.y-ball.y,2))**(0.5)
                 if (distance<(self.R+ball.R)):
                     self.dx, self.dy = -self.dx, -self.dy
                     ball.dx, ball.dy = -ball.dx, -ball.dy
 
+        if (self.x + self.R >= sqreen_X) or (self.x - self.R <= 0):
+            self.dx = - self.dx
+        if (self.y + self.R >= sqreen_Y) or (self.y - self.R <= 0):
+            self.dy = - self.dy
+
         self.x +=self.dx
         self.y +=self.dy
 
-        if (self.x+self.R >=sqreen_X) or (self.x-self.R <=0):
-           self.dx = - self.dx
-        if (self.y+self.R >=sqreen_Y) or (self.y-self.R <=0):
-            self.dy = - self.dy
 
 
 
@@ -74,7 +74,7 @@ def main():
 
     # label = Label(text = "My balls")
     # label.pack(padx=50, pady=50)
-    couple = randint(2,10)
+    couple = randint(2,2)
     balls = [Ball() for i in range(couple)]
     tick()
 
